a8b5173645f4041659e80c6dfaecc69537a87346
Mauro <maurodefalco96@gmail.com>
2025-04-01 01:27:11 +0200

    style: enhance typography in main.css and add description to About page

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,5 +1,6 @@
 <template>
     <AppContainer>
         <h1>About</h1>
+        <p class="max-w-lg text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima numquam magnam hic officiis, odio cumque iste sint! Modi aliquid sed suscipit distinctio! Quibusdam ab dolorem a vel at est porro.</p>
     </AppContainer>
 </template>
