5651531aced3e88dc6b826e5c95b5ba10de79951
Mauro <maurodefalco96@gmail.com>
2025-04-01 01:30:55 +0200

    refactor: replace About page content with reusable IntroContent component

---
+++ app/components/pages/IntroContent.vue
@@ -0,0 +1,10 @@
+<template>
+    <AppContainer>
+        <h1>
+            <slot name="title" />
+        </h1>
+        <p class="max-w-lg text-gray-400">
+            <slot name="description" />
+        </p>
+    </AppContainer>
+</template>
--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,6 +1,7 @@
 <template>
-    <AppContainer>
-        <h1>About</h1>
-        <p class="max-w-lg text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima numquam magnam hic officiis, odio cumque iste sint! Modi aliquid sed suscipit distinctio! Quibusdam ab dolorem a vel at est porro.</p>
-    </AppContainer>
+    <PagesIntroContent>
+        <template #title>About</template>
+        <template #description>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud </template>
+    </PagesIntroContent>
 </template>
