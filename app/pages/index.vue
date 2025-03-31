c233200f0d31376adcb5a0364e2b335e514a843d
Mauro <maurodefalco96@gmail.com>
2025-04-01 00:50:39 +0200

    h1 change

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,12 +9,11 @@ const { data } = await useFetch('https://maurodefalco.it/wp-json/', {
 </script>
 
 <template>
-    <div class="hero-wrapper py-8 text-center px-4 lg:px-10 max-w-[1080px] mx-auto py-8">
-        <div class=" bg-sky-700/85 w-1/3 h-[120px] filter blur-[120px] absolute mix-blend-screen  -z-10 top-4 left-1/2">
+    <div class="hero-wrapper text-center px-4 lg:px-10 max-w-[1080px] mx-auto py-16">
+        <div class="rotate-25 bg-sky-700/95 w-1/3 h-[120px] filter blur-[160px] absolute mix-blend-screen  -z-10 top-16 left-1/3">
         </div>
-        <h1
-            class="text-5xl md:text-7xl  mb-4 font-medium bg-gradient-to-t from-gray-300 to-gray-100 bg-clip-text text-transparent">
-            {{ data.name }}
+        <h1 class="text-5xl md:text-7xl  mb-4 font-medium bg-gradient-to-t from-gray-300 to-gray-100 bg-clip-text text-transparent">
+            Fast, Secure, Customized Web Experiences.
         </h1>
         <p class="text-gray-400 max-w-lg mx-auto">{{ data.description }}</p>
     </div>
