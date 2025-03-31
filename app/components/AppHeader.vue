046c861acb524cdd3123339d2eec91c3e19eeaa4
Mauro <maurodefalco96@gmail.com>
2025-04-01 00:46:14 +0200

    header

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,11 +1,39 @@
+<script setup lang="ts">
+    const { data } = await useFetch('https://maurodefalco.it/wp-json/');
+
+    const isOpen = ref(false);
+    const links = [
+    {
+        key: 'about',
+        label: 'About',
+        url: '/about',
+    },
+    {
+        key: 'blog',
+        label: 'Blog',
+        url: '/blog',
+    },
+    {
+        key: 'projects',
+        label: 'Projects',
+        url: '/projects',
+    },
+    {
+        key: 'contact',
+        label: 'Contact',
+        url: '/contact',
+    }]
+
+</script>
+
 <template>
+
     <header class="p-4 md:p-6">
         <nav aria-label="Main"
             class="flex flex-col mx-auto max-w-6xl justify-between py-2 md:flex-row md:items-center md:justify-between">
             <div class="flex items-center justify-between">
                 <NuxtLink to="/" class="flex items-center z-50" @click="isOpen = false">
-                    <GlideLogo />
-                    <span class="sr-only">{{ settings?.data.site_title }}</span>
+                    {{ data.name }}
                 </NuxtLink>
                 <button class="block md:hidden p-2 text-3xl" :aria-expanded="isOpen" @click="isOpen = true">
                     <Icon name="ph:list-bold" />
@@ -19,17 +47,15 @@
                 </button>
 
                 <ul class="grid justify-items-end gap-6">
-                    <li v-for="link in settings?.data.navigation" :key="link.key">
-                        <PrismicLink :field="link" :class="{ buttonLink: link.variant === 'Button' }"
-                            class="block min-h-11 px-3 text-3xl first:mt-8 " />
+                    <li v-for="link in links" :key="link.key">
+                        <NuxtLink :to="link.url" class="min-h-11 px-3 text-3xl first:mt-8 text-white"></NuxtLink>
                     </li>
                 </ul>
             </div>
 
             <ul class="hidden md:flex items-center gap-6">
-                <li v-for="link in settings?.data.navigation" :key="link.key">
-                    <PrismicLink :field="link" :class="{ buttonLink: link.variant === 'Button' }"
-                        class="inline-flex min-h-11 items-center " />
+                <li v-for="link in links" :key="link.key">
+                    <NuxtLink :to="link.url" class="min-h-11 px-3 font-medium text-white">{{ link.label }}</NuxtLink>
                 </li>
             </ul>
         </nav>
