0ee263304bacfb33309d90b5d0a63538ebe39006
Mauro <maurodefalco96@gmail.com>
2025-04-01 01:32:00 +0200

    refactor: replace static page titles and descriptions with reusable PagesIntroContent component

--- app/pages/blog.vue
+++ app/pages/blog.vue
@@ -1,3 +1,7 @@
 <template>
-    Blog
+    <PagesIntroContent>
+        <template #title>Blog</template>
+        <template #description>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud </template>
+    </PagesIntroContent>
 </template>
