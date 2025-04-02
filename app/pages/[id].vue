a2851212fedc361895b703558a639df113772bf5
Mauro <maurodefalco96@gmail.com>
2025-04-02 23:03:50 +0200

    feat: implement single blog post page with dynamic content fetching

---
+++ app/pages/[id].vue
@@ -0,0 +1,15 @@
+<script setup lanf="ts">
+    
+    const route = useRoute();
+    const slug = route.params.id;
+    const { data: singlePost } = await useFetch<any>(
+    `https://maurodefalco.it/wp-json/wp/v2/posts/${slug}?_embed`); 
+</script>
+
+<template>
+    <PagesIntroContent>
+        <template #title>Single Blog Post</template>
+        <template #description>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud </template>
+    </PagesIntroContent>
+</template>
