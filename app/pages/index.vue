68b6e34cba40a52348384dba4b859b4e22cb942f
Mauro <maurodefalco96@gmail.com>
2025-04-02 23:09:08 +0200

    style: clean up markup and improve formatting in homepage component

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,8 +10,7 @@ const { data } = await useFetch('https://maurodefalco.it/wp-json/', {
 
 <template>
     <div class="hero-wrapper text-center px-4 lg:px-10 max-w-[1080px] mx-auto py-16">
-        <div
-            class="rotate-25 bg-sky-700/95 w-1/3 h-[120px] filter blur-[160px] absolute mix-blend-screen  -z-10 top-16 left-1/3">
+        <div class="rotate-25 bg-sky-700/95 w-1/3 h-[120px] filter blur-[160px] absolute mix-blend-screen -z-10 top-16 left-1/3">
         </div>
         <h1
             class="text-5xl md:text-7xl  mb-4 font-medium bg-gradient-to-t from-gray-300 to-gray-100 bg-clip-text text-transparent">
@@ -22,7 +21,6 @@ const { data } = await useFetch('https://maurodefalco.it/wp-json/', {
             <NuxtLink to="/about" class="primary">About</NuxtLink>
             <NuxtLink to="/contact" class="onlyLink">Contact</NuxtLink>
         </div>
-        
     </div>
 
 
