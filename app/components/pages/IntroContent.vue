466186060293b64772448aa78c12f97e753cd835
Mauro <maurodefalco96@gmail.com>
2025-04-05 19:13:01 +0200

    feat: add dynamic class binding for description paragraph in IntroContent component

--- app/components/pages/IntroContent.vue
+++ app/components/pages/IntroContent.vue
@@ -1,9 +1,19 @@
+<script setup lang="ts">
+ const props = defineProps({
+    length: {
+        type: String,
+        default: 'max-w-lg'
+    },
+ })
+</script>
+
 <template>
     <AppContainer>
         <h1>
             <slot name="title" />
         </h1>
-        <p class="max-w-lg text-gray-400">
+        <p class="text-gray-400"
+            :class="props.length">
             <slot name="description" />
         </p>
     </AppContainer>
