3672d41799bac6c4b9bc0f04ea2728eaf6b6ec21
Mauro <maurodefalco96@gmail.com>
2025-04-05 18:56:38 +0200

    feat: fetch and display featured image in single blog post page

--- app/pages/[id].vue
+++ app/pages/[id].vue
@@ -2,8 +2,12 @@
     
     const route = useRoute();
     const slug = route.params.id;
-    const { data: singlePost } = await useFetch(
-    `https://maurodefalco.it/wp-json/wp/v2/posts?slug=${slug}`); 
+    const { data: singlePost } = await useFetch(`https://maurodefalco.it/wp-json/wp/v2/posts?slug=${slug}`);  
+    const imageFetchUrl = singlePost[0]?.['wp:featuredmedia'][0]?.href;
+
+    console.log(singlePost[0]);
+
+    //const { data: imageData } = await useFetch(imageFetchUrl);
 
 </script>
 
@@ -12,16 +16,21 @@
 
 <template>
 
-<pre>
-{{ singlePost }}
-</pre>
+{{ imageData }}
+{{ imageFetchUrl }}
 
     <PagesIntroContent>
         <template #title>{{ singlePost[0].title.rendered }}</template>
         <template #description>{{ singlePost[0].excerpt.rendered }}</template>
     </PagesIntroContent>
 
-    <div class="container max-w-[1080px] mx-auto prose">
+    <img :src="imageUrl" alt="Post Image" class="mb-4 rounded-lg w-full h-48 object-cover">
+
+    <div class="container max-w-[1080px] mx-auto prose prose-inver">
         {{ singlePost[0].content.rendered }}
     </div>
+
+    <pre>
+{{ singlePost }}
+</pre>
 </template>
