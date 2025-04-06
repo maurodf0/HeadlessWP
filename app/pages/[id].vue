253ec43ed2bf4a4e8c32d230ee2805e91ed4b0fb
Mauro <maurodefalco96@gmail.com>
2025-04-06 23:43:38 +0200

    feat: enhance single post layout and styling for improved readability

--- app/pages/[id].vue
+++ app/pages/[id].vue
@@ -13,9 +13,9 @@
 
 <template>
 
-<pre>
+<!-- <pre>
     {{ singlePost }}
-    </pre>
+    </pre> -->
 
     <PagesIntroContent>
         <template #title>{{ singlePost.title.rendered }}</template>
@@ -25,11 +25,13 @@
 
     </PagesIntroContent>
 
-    <div class="image-container max-w-[1080px] mx-auto">
-        <img :src="singlePost._embedded['wp:featuredmedia'][0]?.source_url" alt="Post Image" class="mb-4 md:mb- p-4 rounded-lg w-full object-cover"> 
+    <article class="!max-w-[880px] mx-auto  prose !prose-invert p-4">
+
+    <div class="image-container mx-auto">
+        <img :src="singlePost._embedded['wp:featuredmedia'][0]?.source_url" alt="Post Image" class="mb-4 md:mb-rounded-lg w-full object-cover"> 
     </div>
 
-    <div class="flex gap-4 md:gap-8 mt-4 justify-between items-center max-w-[1080px] mx-auto">
+    <div class="flex gap-4 md:gap-8 mt-4 justify-between items-center mx-auto">
         <div class="author prose prose-invert flex align-center gap-2">
             Written by: <img :src="singlePost._embedded['author'][0].avatar_urls['96']" class="rounded-full w-8 h-8 mt-0">
           {{ singlePost._embedded['author'][0].name }}
@@ -39,18 +41,18 @@
         </div>
     </div>
 
-    <div v-html="singlePost.content.rendered" class="max-w-[1080px] mx-auto !prose !prose-invert prose-xl p-4">
+    <div v-html="singlePost.content.rendered" class="mx-auto">
        
     </div>
 
-    <div class="artile-meta flex gap-4 justify-between items-center max-w-[1080px] mx-auto">
+    <div class="artile-meta mt-6 flex gap-4 justify-between items-center mx-auto">
         <div class="categories flex gap-2 flex-wrap">
-            <span v-for="category in singlePost._embedded['wp:term'][0]" :key="category.id" class="category bg-gray-100 text-gray-600 px-2 py-1 rounded-full">
+            <span v-for="category in singlePost._embedded['wp:term'][0]" :key="category.id" class="category bg-gray-800 text-gray-300 px-2 py-1 rounded-full">
                 {{ category.name }}
             </span>
         </div>
 
     </div>
-
+</article>
 
 </template>
