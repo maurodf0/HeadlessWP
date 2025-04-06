a376bfbfa5bfc4867f80c52f70fb06a8c5222531
Mauro <maurodefalco96@gmail.com>
2025-04-07 00:56:13 +0200

    feat: update layout structure for improved responsiveness and clarity in post details

--- app/pages/[id].vue
+++ app/pages/[id].vue
@@ -28,7 +28,7 @@
         <img :src="singlePost._embedded['wp:featuredmedia'][0]?.source_url" alt="Post Image" class="mb-4 md:rounded-lg w-full object-cover"> 
     </div>
 
-    <div class="flex gap-4 md:gap-8 mt-4 justify-between items-center mx-auto">
+    <div class="flex flex-col md:flex-row gap-4 md:gap-8 mt-4 justify-between items-center mx-auto">
         <div class="author prose prose-invert flex align-center gap-2">
             Written by: <img :src="singlePost._embedded['author'][0].avatar_urls['96']" class="rounded-full w-8 h-8 mt-0">
           {{ singlePost._embedded['author'][0].name }}
@@ -44,7 +44,7 @@
 </article>
 
     <div class="artile-meta mt-6 max-w-[880px] mx-auto flex gap-4 justify-between items-center px-4 mb-8">
-        <div class="categories flex gap-2 flex-wrap">
+        Tagged in: <div class="categories flex gap-2 flex-wrap">
              <NuxtLink 
                 v-for="category in singlePost._embedded['wp:term'][0]" 
                 :key="category.id" 
