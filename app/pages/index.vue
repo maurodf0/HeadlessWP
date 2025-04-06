9c7a024101fc4c0d183905add71e43137d71ac11
Mauro <maurodefalco96@gmail.com>
2025-04-07 00:52:55 +0200

    feat: adjust hero section layout for improved responsiveness and visual balance

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -11,8 +11,8 @@ const { data } = await useFetch<object>('https://maurodefalco.it/wp-json/', {
 <template>
 
 
-    <div class="hero-wrapper text-center px-4 lg:px-10 max-w-[1080px] mx-auto py-16">
-        <div class="rotate-25 bg-gradient-to-r from-sky-600/95 to-teal-600/95 w-1/3 h-[120px] filter blur-[160px] absolute mix-blend-screen -z-10 top-16 left-1/3">
+    <div class="hero-wrapper text-center px-4 lg:px-10 max-w-[1080px] mx-auto py-6 md:py-16">
+        <div class="rotate-25 bg-gradient-to-r from-sky-600/95 to-teal-600/95 w-full md:w-1/3 h-[120px] filter blur-[160px] absolute mix-blend-screen -z-10 top-16 left-3/3 md:left-1/3">
         </div>
         <h1
             class="text-5xl md:text-7xl  mb-4 font-medium bg-gradient-to-t from-gray-300 to-gray-100 bg-clip-text text-transparent">
