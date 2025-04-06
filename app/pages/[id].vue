cd25038eb9c3d08fd8129599aca4f13eddeb9cb7
Mauro <maurodefalco96@gmail.com>
2025-04-06 23:16:13 +0200

    feat: add author and publication date display to single post page

--- app/pages/[id].vue
+++ app/pages/[id].vue
@@ -9,10 +9,11 @@
 
 </script>
 
-
-
-
 <template>
+
+<pre>
+    {{ singlePost }}
+    </pre>
 
     <PagesIntroContent>
         <template #title>{{ singlePost[0].title.rendered }}</template>
@@ -24,6 +25,15 @@
 
     <div class="image-container max-w-[1080px] mx-auto">
         <img :src="singlePost[0]._embedded['wp:featuredmedia'][0]?.source_url" alt="Post Image" class="mb-4 md:mb- p-4 rounded-lg w-full object-cover"> 
+    </div>
+
+    <div class="flex gap-4 md:gap-8 mt-4 justify-between items-center max-w-[1080px] mx-auto">
+        <div class="author prose prose-invert">
+          By:  {{ singlePost[0]._embedded.author[0].name }}
+        </div>
+        <div class="date prose prose-invert">
+           Published on: {{ singlePost[0].date }}
+        </div>
     </div>
 
     <div v-html="singlePost[0].content.rendered" class="max-w-[1080px] mx-auto !prose !prose-invert prose-xl p-4">
