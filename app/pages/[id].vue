9413793c45a6fdc22f5ce5c75265738163a7d080
Mauro <maurodefalco96@gmail.com>
2025-04-06 23:19:09 +0200

    feat: refactor single post data handling for improved clarity and consistency

--- app/pages/[id].vue
+++ app/pages/[id].vue
@@ -2,9 +2,10 @@
     
     const route = useRoute();
     const slug = route.params.id;
-    const { data: singlePost } = await useFetch(`https://maurodefalco.it/wp-json/wp/v2/posts?slug=${slug}&_embed`);  
+    const { data: singlePosts } = await useFetch(`https://maurodefalco.it/wp-json/wp/v2/posts?slug=${slug}&_embed`);  
     
-    const excerpt = singlePost.value[0].excerpt.rendered.substring(0, 155);
+    const singlePost = singlePosts.value[0];
+    const excerpt = singlePost.excerpt.rendered.substring(0, 155);
 
 
 </script>
@@ -16,7 +17,7 @@
     </pre>
 
     <PagesIntroContent>
-        <template #title>{{ singlePost[0].title.rendered }}</template>
+        <template #title>{{ singlePost.title.rendered }}</template>
         <template #description>
             <div v-html="excerpt"></div>
         </template>
@@ -24,19 +25,19 @@
     </PagesIntroContent>
 
     <div class="image-container max-w-[1080px] mx-auto">
-        <img :src="singlePost[0]._embedded['wp:featuredmedia'][0]?.source_url" alt="Post Image" class="mb-4 md:mb- p-4 rounded-lg w-full object-cover"> 
+        <img :src="singlePost._embedded['wp:featuredmedia'][0]?.source_url" alt="Post Image" class="mb-4 md:mb- p-4 rounded-lg w-full object-cover"> 
     </div>
 
     <div class="flex gap-4 md:gap-8 mt-4 justify-between items-center max-w-[1080px] mx-auto">
         <div class="author prose prose-invert">
-          By:  {{ singlePost[0]._embedded.author[0].name }}
+          By:  {{ singlePost._embedded['author'][0].name }}
         </div>
         <div class="date prose prose-invert">
-           Published on: {{ singlePost[0].date }}
+           Published on: {{ singlePost.date }}
         </div>
     </div>
 
-    <div v-html="singlePost[0].content.rendered" class="max-w-[1080px] mx-auto !prose !prose-invert prose-xl p-4">
+    <div v-html="singlePost.content.rendered" class="max-w-[1080px] mx-auto !prose !prose-invert prose-xl p-4">
        
     </div>
 
