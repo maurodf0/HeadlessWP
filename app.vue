f314b2e0bfc5eb8c6a019d4cce6a5f5d4b5b5e24
Mauro <maurodefalco96@gmail.com>
2025-04-08 01:13:43 +0200

    script for lenis

--- app.vue
+++ app.vue
@@ -1,3 +1,16 @@
+<script setup>
+import { VueLenis, useLenis } from 'lenis/vue'
+import { watch } from 'vue'
+
+const lenis = useLenis(({ scroll }) => {
+  // called every scroll
+})
+
+watch(lenis, (lenis) => {
+  // lenis instance
+})
+</script>
+
 <template>
     <VueLenis root>
     <NuxtLayout>
