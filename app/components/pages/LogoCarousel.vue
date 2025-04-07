2ca56aeb6c05db47a6104699c724163359d77203
Mauro <maurodefalco96@gmail.com>
2025-04-07 23:31:19 +0200

    feat: add new TechLogo components for enhanced branding in LogoCarousel

--- app/components/pages/LogoCarousel.vue
+++ app/components/pages/LogoCarousel.vue
@@ -2,11 +2,13 @@
 </script>
 
 <template>
-    <div class="flex gap-8 md:gap-16 justify-center items-center opacity-45">
+    <div class="md:flex gap-4 md:gap-16 md:justify-center md:items-center opacity-45 bg-gradient-to-l from-bg-green-900 to-bg-transparent max-w-[1080px] mx-auto flex-wrap p-4 md:pr-0 grid grid-cols-3 place-items-center">
         <TechLogoVue class="w-8 h-8 md:w-12 md:h-12" /> 
         <TechLogoNuxt class="w-8 h-8 md:w-12 md:h-12" />
         <TechLogoPhp class="w-8 h-8 md:w-12 md:h-12" />
         <TechLogoGsap class="w-8 h-8 md:w-12 md:h-12" />
+        <TechLogoTailwind class="w-8 h-8 md:w-12 md:h-12" />
+        <TechLogoWordpress class="w-8 h-8 md:w-12 md:h-12" />
         <TechLogoThree class="w-8 h-8 md:w-12 md:h-12" />
         <TechLogoFigma class="w-8 h-8 md:w-12 md:h-12" />
     </div>
