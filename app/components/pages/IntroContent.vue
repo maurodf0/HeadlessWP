a8870913a511dabfaf19a9aeb9846d345006002a
Mauro <maurodefalco96@gmail.com>
2025-04-08 11:58:17 +0200

    feat: add background gradient effect to IntroContent component

--- app/components/pages/IntroContent.vue
+++ app/components/pages/IntroContent.vue
@@ -10,6 +10,8 @@ const { length } = props
 </script>
 
 <template>
+          <div class="rotate-25 bg-gradient-to-r from-sky-600/95 to-teal-600/95 w-full md:w-1/3 h-[120px] filter blur-[160px] absolute mix-blend-screen -z-10 top-16 left-3/3 md:left-3/3">
+        </div>
     <AppContainer>
         <h1>
             <slot name="title" />
