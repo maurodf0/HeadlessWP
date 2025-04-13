745be81af455b0fdf5556926a37a3783f5da1bc1
Mauro <maurodefalco96@gmail.com>
2025-04-14 00:11:29 +0200

    fix overflow-x bug

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -197,7 +197,7 @@ const projects = [
     <AppLoader :loading="loading" />
 
     <div class="hero-wrapper text-center px-4 lg:px-10 max-w-[90%] md:max-w-[1080px] mx-auto py-6 md:py-16">
-        <div class="glow-effect rotate-25 bg-gradient-to-r from-sky-600/95 to-teal-600/95 w-1/2 md:w-full h-[120px] filter blur-[160px] absolute mix-blend-screen -z-10 top-16 left-3/3 md:left-1/3"></div>
+        <div class="glow-effect rotate-25 bg-gradient-to-r from-sky-600/95 to-teal-600/95 w-1/2  h-[120px] filter blur-[160px] absolute mix-blend-screen -z-10 top-16 left-3/3 md:left-1/3"></div>
           <div class="rotate-15 bg-gradient-to-r from-purple-600/65 to-blue-600/20 w-full md:w-1/2 h-[200px] filter blur-[180px] absolute mix-blend-screen top-20 right-11 glow-ball"></div>
 
         <div class="overflow-hidden">
