e1d2b9d3b010d70d2330366f832ee543b2cbe2b5
Mauro <maurodefalco96@gmail.com>
2025-04-17 11:23:38 +0200

    shadow over service card

--- app/components/ServicesCard.vue
+++ app/components/ServicesCard.vue
@@ -19,8 +19,10 @@
 
 <template>
   
+
 <div v-for="service in services" :key="service.id" 
-class="services-card flex flex-col gap-8 p-4 md:p-8 rounded-lg bg-gray-950 hover:scale-105 hover:rotate-3 transition-all hover:border hover:border-sky-400" >
+class="services-card flex flex-col gap-8 p-4 md:p-8 rounded-lg bg-gray-950 hover:scale-105 hover:rotate-3 transition-all hover:border hover:border-sky-400 relative" >
+<div class="absolute inset-0 z-20 bg-[radial-gradient(40%_128px_at_50%_0%,theme(backgroundColor.white/5%),transparent)]"></div>
 
     <div class="flex flex-col justify-between">
         <h4 class="font-medium mb-4">{{ service.title }}</h4>
