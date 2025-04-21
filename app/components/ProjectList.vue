de534d3b0b08379fe8dddb453fc11c4ed0036e19
Mauro <maurodefalco96@gmail.com>
2025-04-22 01:28:11 +0200

    Github projet init

---
+++ app/components/ProjectList.vue
@@ -0,0 +1,29 @@
+<script setup>
+const { error, pending, data: repos } = await useFetch('https://api.github.com/users/maurodf0/repos');
+//display only repo with description and sorted by stars number
+const reposWithDescription = computed(() => repos.value.filter(repo => repo.description).sort((a, b) => b.stargazers_count - a.stargazers_count))
+
+</script>
+
+<template>
+  <div class="">
+    <section v-if="pending">Loading...</section>
+    <section v-else-if="error">Something went wrong, try again...</section>
+    <section v-else>
+      <ul class="grid grid-cols-1 gap-4">
+        <li v-for="repo in reposWithDescription"
+          class="border border-gray-200 dark:border-gray-700 rounded-sm p-4 hover:bg-gray-100 dark:hover:bg-gray-800"
+          :key="repo.id">
+          <a :href="repo.html_url" target="_blank">
+            <div class="flex items-center justify-between text-sm">
+              <div class="font-semibold">{{ repo.name }}</div>
+            
+            </div>
+            <p class="text-sm mb-0" >{{ repo.description }}</p>
+          </a>
+        </li>
+      </ul>
+    </section>
+  </div>
+</template>
+
--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -4,4 +4,9 @@
         <template #description>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
             ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud </template>
     </PagesIntroContent>
+
+
+    <div class="max-w-[880px] mx-auto p-4 md:p-0">
+        <ProjectList />
+    </div>
 </template>
