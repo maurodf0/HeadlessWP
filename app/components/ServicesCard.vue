5d6cf22d1b89044b21c7e4d608f56d2d9d3cc04e
Mauro <maurodefalco96@gmail.com>
2025-04-23 01:57:46 +0200

    remove GSAP Animation (for now)

--- app/components/ServicesCard.vue
+++ app/components/ServicesCard.vue
@@ -14,45 +14,13 @@
     import { ScrollTrigger } from "gsap/ScrollTrigger";
     gsap.registerPlugin(ScrollTrigger);
 
-    onMounted(() => {
-
- const services = gsap.utils.toArray('.services-card') as HTMLElement[];
-            const ServiceTl = gsap.timeline({
-            scrollTrigger: {
-                trigger: '.services-container',
-                start: '-=500px top',
-                end: '500px bottom',
-                scrub: 1,
-                markers : true,
-            }
-    })
-
-   
-        services.forEach((el, index) => {
-            ServiceTl.from(el, {
-                opacity: 0,
-                duration: 1,
-                y: 100,
-                ease: 'power3.inOut',
-                stagger: {
-                    amount: 0.2,
-                    from: 'start',
-                    ease: 'power3.inOut',
-                },
-            });
-
-        
-      })
-    })
-
-
 </script>
 
 <template>
   
 
 <div v-for="service in services" :key="service.id" 
-class="services-card z-10 flex flex-col gap-8 p-4 md:p-8 rounded-lg bg-gray-950 hover:scale-105 hover:rotate-3 transition-all hover:border hover:border-sky-400 relative" >
+class="services-card z-10 flex flex-col gap-8 p-4 md:p-8 rounded-lg bg-gray-950 hover:scale-105 hover:rotate-3 transition-all border-2 border-gray-100/15 hover:border-sky-400 relative" >
     <div class="flex flex-col justify-between">
       <Icon class="mb-2" relative :name="service.icon" size="xl" />     
         <h4 class="font-medium mb-4">{{ service.title }}</h4>
