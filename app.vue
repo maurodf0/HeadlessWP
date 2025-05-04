55423320aa1b49ad8527910dcf966a4b9e1e202a
Mauro <maurodefalco96@gmail.com>
2025-05-05 01:44:44 +0200

    better error handling

--- app.vue
+++ app.vue
@@ -1,40 +1,5 @@
-<script setup>
-import { gsap } from 'gsap'
-
-const onEnter = (el, done) => {
-  console.log('enter triggered')
-  gsap.fromTo(el, { opacity: 0, y: 100 }, {
-    opacity: 1, y: 0, duration: 1, onComplete: done
-  })
-}
-
-const onLeave = (el, done) => {
-  console.log('leave triggered')
-  gsap.to(el, { opacity: 0, y: -100, duration: 1, onComplete: done })
-}
-</script>
-
 <template>
-  <Transition mode="out-in" @enter="onEnter" @leave="onLeave">
-      <div :key="$route.fullPath">
       <NuxtLayout>
         <NuxtPage />
       </NuxtLayout>
-      </div>
-    </Transition>
-
 </template>
-
-
-<style scoped>
-.page-enter-active,
-.page-leave-active {
-  position: absolute;
-  width: 100%;
-}
-
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-}
-</style>
--- app/pages/[id].vue
+++ app/pages/[id].vue
@@ -25,10 +25,13 @@ interface WpPost {
     
     const route = useRoute();
     const slug = route.params.id as string;
-    const { data: singlePosts, loading, error } = await useFetch<WpPost[]>(`https://wp.maurodefalco.it/wp-json/wp/v2/posts?slug=${slug}&_embed`);  
-if (error) {
-  throw createError({ statusCode: 404, statusMessage: 'Contenuto non trovato' })
-}
+    const { data: singlePosts, loading, error } = await useFetch<WpPost[]>(`https://wp.maurodefalco.it/wp-json/wp/v2/posts?slug=${slug}&_embed`); 
+
+if (!singlePosts.value[0]) {
+      console.error('Error fetching post:', error);
+      throw createError({ statusCode: 404})
+    }
+    console.log('Single post data:', singlePosts.value);
     const singlePost = singlePosts.value?.[0];
     const excerpt = singlePost?.excerpt.rendered;
 
@@ -42,47 +45,51 @@ if (error) {
 
 <template>
 
+  <div class="single-wrapper">
     <PagesIntroContent length="max-w-full">
-        <template #title>{{ singlePost?.title.rendered }}</template>
-        <template #description>
-            <em><div v-html="excerpt"></div></em>
-        </template>
+      <template #title>{{ singlePost?.title.rendered }}</template>
+      <template #description>
+        <em>
+          <div v-html="excerpt"></div>
+        </em>
+      </template>
     </PagesIntroContent>
 
     <article class="!max-w-[880px] mx-auto prose !prose-invert p-4">
 
-    <div class="image-container mx-auto">
-        <img v-if="singlePost?._embedded['wp:featuredmedia']" :src="singlePost?._embedded['wp:featuredmedia'][0]?.source_url" alt="Post Image" class="mb-4 md:rounded-lg w-full object-cover"> 
-    </div>
+      <div class="image-container mx-auto">
+        <img v-if="singlePost?._embedded['wp:featuredmedia']"
+          :src="singlePost?._embedded['wp:featuredmedia'][0]?.source_url" alt="Post Image"
+          class="mb-4 md:rounded-lg w-full object-cover">
+      </div>
 
-    <div class="flex flex-col md:flex-row gap-4 md:gap-8 mt-4 justify-between items-center mx-auto">
+      <div class="flex flex-col md:flex-row gap-4 md:gap-8 mt-4 justify-between items-center mx-auto">
         <div class="author prose prose-invert flex align-center gap-2">
-            Written by: <img :src="singlePost?._embedded?.['author'][0]?.avatar_urls['96']" class="rounded-full w-8 h-8 mt-0">
+          Written by: <img :src="singlePost?._embedded?.['author'][0]?.avatar_urls['96']"
+            class="rounded-full w-8 h-8 mt-0">
           {{ singlePost?._embedded?.['author'][0]?.name }}
         </div>
         <div class="date prose prose-invert">
-           Published on: {{ new Date(singlePost?.date).toLocaleDateString('it-IT', { year: 'numeric', month: 'long', day: 'numeric' }) }}
+          Published on: {{ new Date(singlePost?.date).toLocaleDateString('it-IT', { year: 'numeric', month: 'long', day:
+          'numeric' }) }}
         </div>
-    </div>
+      </div>
 
-    <div v-html="singlePost?.content.rendered" class="mx-auto">
-       
-    </div>
-</article>
+      <div v-html="singlePost?.content.rendered" class="mx-auto">
+
+      </div>
+    </article>
 
     <div class="artile-meta mt-6 max-w-[880px] mx-auto flex gap-4 justify-between items-center px-4 mb-8">
-        Tagged in: <div class="categories flex gap-6 md:gap-2 flex-wrap">
-             <NuxtLink 
-                v-for="category in singlePost?._embedded?.['wp:term'][0]" 
-                :key="category.id" 
-                :to="`/category/${category.slug}`"
-                class="text-decoration-none"> 
-                <span class="category bg-gray-800 text-gray-200 px-4 py-2 rounded-full hover:bg-sky-400/95 font-normal">
-                    {{ category.name }}
-                </span>
-            </NuxtLink>
-        </div>
+      Tagged in: <div class="categories flex gap-6 md:gap-2 flex-wrap">
+        <NuxtLink v-for="category in singlePost?._embedded?.['wp:term'][0]" :key="category.id"
+          :to="`/category/${category.slug}`" class="text-decoration-none">
+          <span class="category bg-gray-800 text-gray-200 px-4 py-2 rounded-full hover:bg-sky-400/95 font-normal">
+            {{ category.name }}
+          </span>
+        </NuxtLink>
+      </div>
 
     </div>
-
+  </div>
 </template>
