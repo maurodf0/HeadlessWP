02ad516f2c5528988a2c9ce9098159f37246bcd7
Mauro <maurodefalco96@gmail.com>
2025-05-14 23:40:41 +0200

    page title into page animation

--- app/components/pages/AppTransition.vue
+++ app/components/pages/AppTransition.vue
@@ -4,14 +4,22 @@ import { gsap } from 'gsap';
 const TransitionEl1 = ref<HTMLElement | null>(null);
 const TransitionEl2 = ref<HTMLElement | null>(null);
 const TransitionEl3 = ref<HTMLElement | null>(null);
+const loadingText = ref(''); // Ref per il testo dinamico
 
 const animationIn = () => {
   const tl = gsap.timeline();
   return tl
     .set([TransitionEl1.value, TransitionEl2.value, TransitionEl3.value], { y: '100%' })
+    .set('.text-loader', { opacity: 0, y: -50 }) // Reset stato
     .to(TransitionEl3.value, { y: 0, duration: 0.65, ease: 'power4.inOut' }, 0)
     .to(TransitionEl2.value, { y: 0, duration: 0.75, ease: 'power4.inOut' }, 0)
-    .to(TransitionEl1.value, { y: 0, duration: 0.85, ease: 'power4.inOut' }, 0);
+    .to(TransitionEl1.value, { y: 0, duration: 0.85, ease: 'power4.inOut' }, 0)
+    .to('.text-loader', {
+      opacity: 1,
+      duration: 0.85,
+      ease: 'power4.inOut',
+      y: 0
+    }, '-=0.5');
 };
 
 const animeOut = () => {
@@ -23,6 +31,13 @@ const animeOut = () => {
       ease: 'power4.inOut',
       onComplete: () => gsap.set(TransitionEl3.value, { y: '100%' })
     }, 0)
+    .to('.text-loader', {
+      opacity: 0,
+      duration: 0.85,
+      ease: 'power4.inOut',
+      y: 50,
+      onComplete: () => gsap.set('.text-loader', { y: -50, opacity: 0 })
+    })
     .to(TransitionEl2.value, {
       y: '-100%',
       duration: 0.95,
@@ -41,40 +56,46 @@ const route = useRoute();
 const router = useRouter();
 
 onMounted(() => {
-  // Transizione di ingresso PRIMA del cambio pagina
   router.beforeEach(async (to, from, next) => {
+    loadingText.value = to.meta.title || to.name?.toString() || 'Loading'; // fallback
     await animationIn();
     next();
   });
 
-  // Aspettiamo il DOM pronto prima di eseguire la transizione di uscita
-  // e poi eseguiamo la transizione di uscita
   watch(
     () => route.fullPath,
     async () => {
-      await nextTick(); // Next tick per assicurarsi che il DOM sia pronto e che la ref (route in questo caso) sia aggiornata
+      await nextTick();
       animeOut();
     }
   );
 });
 </script>
 
+
 <template>
   <div>
     <div
       ref="TransitionEl1"
       class="fixed bottom-0 left-0 w-full h-full z-[9999] bg-gray-950 pointer-events-none"
-      style="transform: translateY(100%);"
-    />
+      style="transform: translateY(100%)"
+    >
+      <div class="overflow-hidden h-full w-full">
+        <span
+          class="uppercase text-[60px] md:text-[220px] text-gray-900 bottom-5 right-5 absolute text-loader"
+          v-text="loadingText"
+        />
+      </div>
+    </div>
     <div
       ref="TransitionEl2"
       class="fixed bottom-0 left-0 w-full h-full z-[9998] bg-teal-400 pointer-events-none"
-      style="transform: translateY(100%);"
+      style="transform: translateY(100%)"
     />
     <div
       ref="TransitionEl3"
       class="fixed bottom-0 left-0 w-full h-full z-[9997] bg-gray-900 pointer-events-none"
-      style="transform: translateY(100%);"
+      style="transform: translateY(100%)"
     />
   </div>
 </template>
