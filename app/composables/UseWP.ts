76f3296c73453226ca836f448ab4d2b43298e903
Mauro <maurodefalco96@gmail.com>
2025-11-10 13:47:46 +0100

    useWP Composable

---
+++ app/composables/UseWP.ts
@@ -0,0 +1,44 @@
+interface WpPost {
+  id: number;
+  title: { rendered: string };
+  excerpt: { rendered: string };
+  content: { rendered: string };
+  date: string;
+  _embedded: {
+    'wp:featuredmedia': Array<{
+      source_url: string;
+    }>;
+    author: Array<{
+      name: string;
+      avatar_urls: { [key: string]: string };
+    }>;
+    'wp:term': Array<Array<{
+      id: number;
+      name: string;
+      slug: string;
+    }>>;
+  };
+}
+
+const API_URL:string = "https://wp.maurodefalco.it/wp-json/wp/v2/"
+
+export default const UseWP = () => {
+
+    const getPostBySlug = async (slug: string) => {
+    const res = await fetch(`${API_URL}/posts?slug=${slug}`)
+    if (!res.ok) throw new Error("Errore nel recupero del post")
+    const data = await res.json()
+    return data[0]
+  }
+
+  const getCategories
+  const getPages
+
+  const { data: singlePosts, loading, error } = await useFetch<WpPost[]>(`${API_URL}posts?slug=${slug}&_embed`); 
+
+  return {
+  getPostBySlug,
+    getCategories,
+    getPages
+  }
+}
